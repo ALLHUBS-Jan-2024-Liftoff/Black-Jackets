e4025cb86ca939cfce3f4436c3a17ee6df69c3cf
fowlerrc <142541725+fowlerrc@users.noreply.github.com>
2024-07-04 20:40:26 -0500

    Added API

---
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/BlackJacketsApplication.java
@@ -0,0 +1,13 @@
+package BlackJackets.BlackJackets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackJacketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackJacketsApplication.class, args);
+	}
+
+}
---
+++ BlackJackets API/src/test/java/BlackJackets/BlackJackets/BlackJacketsApplicationTests.java
@@ -0,0 +1,13 @@
+package BlackJackets.BlackJackets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackJacketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
