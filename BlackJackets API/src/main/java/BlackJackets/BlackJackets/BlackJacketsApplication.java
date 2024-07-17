ac7366fe95b409449c2f47a56bf8c25a5be7e554
Μιχαίας <mmcadoulos@gmail.com>
2024-07-16 20:15:47 -0500

    fixed boot error

--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/BlackJacketsApplication.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/BlackJacketsApplication.java
@@ -16,20 +16,20 @@ public class BlackJacketsApplication {
 		SpringApplication.run(BlackJacketsApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return args -> {
+//
+//			System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
+//
+//		};
+//	}
 
 
 }
