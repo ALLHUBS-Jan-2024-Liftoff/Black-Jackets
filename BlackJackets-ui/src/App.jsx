c76595ad71af12fae1432556e331d84e211bf3fc
Μιχαίας <mmcadoulos@gmail.com>
2024-07-25 16:50:23 -0500

    Added Add Gig Page to router and temporary link in NavBar

--- BlackJackets-ui/src/App.jsx
+++ BlackJackets-ui/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
-import './App.css'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import About from './pages/About'
-import RegisterPage from './pages/RegisterPage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import VenueList from './pages/VenueList'
-import GigPage from './pages/GigPage'
+import { useState } from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RegisterPage from "./pages/RegisterPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VenueList from "./pages/VenueList";
+import GigPage from "./pages/GigPage";
 
 function App() {
-  
   // const [gigs, setGigs] = useState();
 
   // const addGig(name, date, genre, ages, headliner, bandSlots) => {
@@ -27,11 +26,11 @@ function App() {
             <Route path="about" element={<About />} />
             <Route path="register" element={<RegisterPage />} />
             {/* <Route path="contact" element={<Contact />} /> */}
+            <Route path="gigs/add" element={<GigPage />} />
           </Route>
         </Routes>
       </Router>
       <VenueList />
-      <GigPage />
     </>
   );
 }
