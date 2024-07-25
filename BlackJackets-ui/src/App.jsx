beb44e74bbe2bd86b2874540b613ec11d1bf03f9
Matt Stephens <mdstephens33@hotmail.com>
2024-07-24 20:10:56 -0500

    added register page, register form, and register service

--- BlackJackets-ui/src/App.jsx
+++ BlackJackets-ui/src/App.jsx
@@ -4,6 +4,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import About from './pages/About'
+import RegisterPage from './pages/RegisterPage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import VenueList from './pages/VenueList'
 
@@ -13,18 +14,19 @@ function App() {
   return (
     // <Home />
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navbar />} >
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          {/* <Route path="contact" element={<Contact />} /> */}
-        </Route>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="register" element={<RegisterPage />} />
+            {/* <Route path="contact" element={<Contact />} /> */}
+          </Route>
         </Routes>
-    </Router>
+      </Router>
       <VenueList />
-      </>
-  )
+    </>
+  );
 }
 
 export default App;
