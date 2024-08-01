59a66d14d4e4744ba3083546a25ecf7b5c9da87f
Μιχαίας <mmcadoulos@gmail.com>
2024-08-01 17:13:48 -0500

    Added comment notes on each mapping

--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/controllers/GigController.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/controllers/GigController.java
@@ -21,16 +21,19 @@ public class GigController {
         return gigRepository.save(newGig);
     }
 
+    // Get all Gigs
     @GetMapping("all")
     public List<Gig> findAllGigs() {
         return gigRepository.findAll();
     }
 
+    // Get one Gig
     @GetMapping("{id}")
     public Gig findGigById(@PathVariable("id") Long gigId) {
         return gigRepository.findById(gigId).get();
     }
 
+    //Get all Gigs owned by one Venue
     // Will Finish after ORM Mapping
 //    @GetMapping()
 //    public List<Gig> findGigsByVenue(@RequestParam int venueId){
