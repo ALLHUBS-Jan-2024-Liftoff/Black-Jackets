e398fd38c38f7f8d9fe026bef86edf575dd4d65d
Matt Stephens <mdstephens33@hotmail.com>
2024-08-03 11:44:18 -0500

    merged

--- BlackJackets-ui/src/App.jsx
+++ BlackJackets-ui/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
+export default App;
+import { useState } from 'react';
+import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import About from './pages/About'
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import VenueList from "./pages/VenueList";
-import GigPage from "./pages/GigPage";
-import CreateGigForm from "./components/CreateGig";
+import RegisterPage from './pages/RegisterPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import VenueList from './pages/VenueList'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
   return (
     // <Home />
