edabc80f08ca2ec32c75f034925e9530abf694ad
fowlerrc <142541725+fowlerrc@users.noreply.github.com>
2024-08-07 13:14:58 -0500

    youtube video upload htlm

--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/BlackJacketsApplication.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/BlackJacketsApplication.java
@@ -2,16 +2,18 @@ package BlackJackets.BlackJackets;
 
 import BlackJackets.BlackJackets.data.ReviewRepo;
 import BlackJackets.BlackJackets.models.Reviews;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
 public class BlackJacketsApplication {
 
 	public static void main(String[] args) throws Throwable {
-		SpringApplication.run(BlackJacketsApplication.class, args);
+		SpringApplication.run(BlackJacketsApplication.class, (args));
 
 		ReviewRepo reviewRepo = new ReviewRepo();
 		reviewRepo.addReview(new Reviews("TheBand", "The venue was awesome and the crowd was electric", 5));
