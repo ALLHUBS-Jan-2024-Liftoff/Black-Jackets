a0df5493762c2a1db2cdb2c0a735580d486e5b4c
Μιχαίας <mmcadoulos@gmail.com>
2024-08-11 16:08:57 -0500

    Cleaned up code in GigRepository

--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/data/GigRepository.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/data/GigRepository.java
@@ -3,8 +3,6 @@ package BlackJackets.BlackJackets.data;
 
 import BlackJackets.BlackJackets.models.Gig;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
