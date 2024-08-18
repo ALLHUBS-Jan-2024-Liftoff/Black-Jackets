4d1ae1ea1a6d15f0fab9981438d6746c876876e1
revathi <arevathi125@gmail.com>
2024-08-17 20:39:12 -0500

    Navbar changes committed

--- BlackJackets-ui/src/components/Navbar.jsx
+++ BlackJackets-ui/src/components/Navbar.jsx
@@ -1,69 +1,78 @@
 import React from "react";
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
+import Logout from "../pages/Logout";
 
 function Navbar() {
+  const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <>
       <nav>
         <ul className="nav nav-pills flex-column flex-sm-row">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/gigs/search">
-              Search Gigs
-            </Link>
-          </li>
-          <li className="nav-item">
+          {!authenticated ? (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/gigs/search">
+                  Search Gigs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  onClick={Logout(authenticated)}
+                  to="/login">
+                  Logout
+                </Link>
+              </li>
+              {/* <li className="nav-item">
             <Link className="nav-link" to="/gigs/add">
               Add Gig Test
             </Link>
-          </li>
-          {/* <li className="nav-item">
+          </li> */}
+              {/* <li className="nav-item">
                       <Link className="nav-link" to="/venue-list">Venue List</Link>
                   </li> */}
-          <li className="nav-item">
+              {/* <li className="nav-item">
             <Link className="nav-link" to="/add-venue">
               Create Venue
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/venue-dashboard">
-              Venue User Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/logout">
-              Logout
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/guest-view">
-              Guest View
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/message-list">
-              Notification
-            </Link>
-          </li>
+          </li> */}
+              {/* <li className="nav-item">
+                <Link className="nav-link" to="/guest-view">
+                  Guest View
+                </Link>
+              </li> */}
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <Link className="nav-link" to="/venue-dashboard">
+                  Venue User Dashboard
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
       <Outlet />
