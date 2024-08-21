c1cda75125083ad94af10764890b8333f19fa7c7
revathi <arevathi125@gmail.com>
2024-08-20 19:30:10 -0500

    Message controller committed

--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/Controllers/MessageController.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/Controllers/MessageController.java
@@ -1,2 +1,34 @@
-package BlackJackets.BlackJackets.Controllers;public class MessageController {
+package BlackJackets.BlackJackets.Controllers;
+
+import BlackJackets.BlackJackets.dto.MessageDTO;
+import BlackJackets.BlackJackets.models.Message;
+import BlackJackets.BlackJackets.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("message")
+@CrossOrigin(origins = "http://localhost:5173")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    // Create Message
+    @PostMapping
+    public ResponseEntity<String> createMessage(@RequestBody MessageDTO messageDTO) throws IOException{
+        String message = this.messageService.createNewMessage(messageDTO);
+        return new ResponseEntity<>(message, HttpStatusCode.valueOf(200));
+    }
+
+    // Get All Messages By Venue Id
+    @GetMapping("allMessages/{venueId}")
+    public ResponseEntity<List<Message>> getAllMessagesByVenueId(@PathVariable Integer venueId){
+        return new ResponseEntity<>(this.messageService.getAllMessagesByVenueId(venueId),HttpStatusCode.valueOf(200));
+    }
 }
--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/service/impl/MessageServiceImpl.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/service/impl/MessageServiceImpl.java
@@ -1,2 +1,36 @@
-package BlackJackets.BlackJackets.service.impl;public class MessageServiceImpl {
+package BlackJackets.BlackJackets.service.impl;
+
+import BlackJackets.BlackJackets.data.MessageRepo;
+import BlackJackets.BlackJackets.dto.MessageDTO;
+import BlackJackets.BlackJackets.models.Message;
+import BlackJackets.BlackJackets.service.MessageService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MessageServiceImpl implements MessageService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    // Create Message
+    @Override
+    public String createNewMessage(MessageDTO messageDTO) {
+        Message message = this.modelMapper.map(messageDTO, Message.class);
+        this.messageRepo.save(message);
+        return "Message added successfully";
+    }
+
+    @Override
+    public List<Message> getAllMessagesByVenueId(int venueId) {
+        List<Message> messageList = this.messageRepo.findByVenueId(venueId);
+        return messageList;
+    }
+
 }
