36258b7b8447dbe64b4d964f25942d6cfe28421f
revathi <arevathi125@gmail.com>
2024-08-28 12:54:33 -0500

    committed changes

--- BlackJackets API/src/main/java/BlackJackets/BlackJackets/Controllers/YouTubeController.java
+++ BlackJackets API/src/main/java/BlackJackets/BlackJackets/Controllers/YouTubeController.java
@@ -3,13 +3,11 @@ package BlackJackets.BlackJackets.Controllers;
 import BlackJackets.BlackJackets.service.YouTubeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/youtube")
+@CrossOrigin(origins = "http://localhost:5173")
 public class YouTubeController {
 
     @Autowired
--- BlackJackets-ui/src/pages/Youtube.jsx
+++ BlackJackets-ui/src/pages/Youtube.jsx
@@ -10,8 +10,8 @@ const Youtube = () => {
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen></iframe>
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen></iframe>
     </div>
   );
 };
